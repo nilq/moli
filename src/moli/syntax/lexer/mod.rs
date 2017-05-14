34e466e88f1c6a8c3992aeccb3fbd6b3e7cba43c
nilq <i.am@nilq.dk>
2017-05-14 14:05:42 +0200

    Add block tree generator

--- src/moli/syntax/lexer/mod.rs
+++ src/moli/syntax/lexer/mod.rs
@@ -1,7 +1,9 @@
 pub mod token;
 pub mod tokenizer;
 pub mod matcher;
+pub mod block_tree;
 
 pub use self::token::{Token, TokenType, TokenPosition};
 pub use self::matcher::Matcher;
 pub use self::tokenizer::Tokenizer;
+pub use self::block_tree::BlockTree;
